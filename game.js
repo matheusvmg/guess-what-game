3a238b72ad352e3685da4dce8966f1c0b416326e
Matheus Góes <matheusvmg@gmail.com>
2020-11-26 10:14:17 -0300

    changed the animals image and remove unnecessary files

--- game.js
+++ game.js
@@ -1,20 +1,37 @@
 //get the cards
-const card = document.querySelectorAll('.card');
+const card = document.querySelectorAll(".card");
 
 //set the word list
-const fruits = ['apple', 'watermelon', 'orange', 'pear', 'cherry', 'strawberry', 'nectarine', 'grape', 'mango'];
-const animals = ['dog', 'cat', 'cow', 'sheep', 'rabbit', 'duck', 'horse', 'pig', 'chicken'];
-const colors = ['black', 'white', 'green', 'blue', 'red', 'pink', 'orange', 'brown', 'gray '];
-
-//get the value of the inputs
-function getInputValues(value){
-    card.forEach(c => {
-        if(c.getAttribute('data-value') === value){
-            console.log('match');
-            return;
-        }else{
-            console.log('does not match');
-            return;
-        };
-    });
-};
+const fruits = [
+  "apple",
+  "watermelon",
+  "orange",
+  "pear",
+  "cherry",
+  "strawberry",
+  "nectarine",
+  "grape",
+  "mango",
+];
+const animals = [
+  "dog",
+  "cat",
+  "cow",
+  "sheep",
+  "rabbit",
+  "duck",
+  "horse",
+  "pig",
+  "chicken",
+];
+const colors = [
+  "black",
+  "white",
+  "green",
+  "blue",
+  "red",
+  "pink",
+  "orange",
+  "brown",
+  "gray ",
+];
