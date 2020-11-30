11ddd1c76b382912cdff09d259e5bbd520372502
Matheus Góes <matheusvmg@gmail.com>
2020-11-30 19:26:11 -0300

    first implementation/test logic game

--- game.js
+++ game.js
@@ -1,2 +1,16 @@
 //get the cards
-const card = document.querySelectorAll(".card");
+const card = document.getElementsByClassName("card");
+
+//get the input
+const input = document.getElementsByClassName('answer');
+
+//words
+const fruits = ['pineapple', 'watermelon', 'lemon', 'pear', 'apple', 'strawberry', 'grape', 'mango', 'banana'];
+
+function getInputValues(value){
+    for(let i = 0; i < card.length; i++){
+        if(fruits.includes(value) && card[i].getAttribute('data-value') === value){
+            card[i].setAttribute('class', 'success');
+        }
+    };
+};
