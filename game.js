1d0d7a4235ce19ddc7608659bfa7f5f4ee7405f9
Matheus Góes <matheusvmg@gmail.com>
2020-11-30 20:27:59 -0300

    making the card readonly when the player type the right word

--- game.js
+++ game.js
@@ -1,15 +1,14 @@
 //get the cards
 const card = document.getElementsByClassName("card");
 
-//get the input
-const input = document.getElementsByClassName('answer');
+const words = document.getElementsByClassName('w');
 
 //words
 const fruits = ['pineapple', 'watermelon', 'lemon', 'pear', 'apple', 'strawberry', 'grape', 'mango', 'banana'];
 
 function getInputValues(value){
     for(let i = 0; i < card.length; i++){
-        if(fruits.includes(value) && card[i].getAttribute('data-value') === value){
+        if(fruits.includes(value.toLowerCase()) && card[i].getAttribute('data-value') === value.toLowerCase()){
             card[i].setAttribute('class', 'success');
         }
     };
