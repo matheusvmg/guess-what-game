91e3d05dae7c4b6a55df4a6394f70ee047316713
Matheus Góes <matheusvmg@gmail.com>
2020-12-01 00:07:28 -0300

    implemented the logic and redirect to success page when the user completes the game

--- game.js
+++ game.js
@@ -1,15 +1,33 @@
 //get the cards
 const card = document.getElementsByClassName("card");
 
-const words = document.getElementsByClassName('w');
+const words = document.getElementsByClassName("w");
 
 //words
-const fruits = ['pineapple', 'watermelon', 'lemon', 'pear', 'apple', 'strawberry', 'grape', 'mango', 'banana'];
-
-function getInputValues(value){
-    for(let i = 0; i < card.length; i++){
-        if(fruits.includes(value.toLowerCase()) && card[i].getAttribute('data-value') === value.toLowerCase()){
-            card[i].setAttribute('class', 'success');
-        }
-    };
-};
+const fruits = [
+  "pineapple",
+  "watermelon",
+  "lemon",
+  "pear",
+  "apple",
+  "strawberry",
+  "grape",
+  "mango",
+  "banana",
+];
+var rightAnswers = 0;
+function getInputValues(value) {
+  for (let i = 0; i < card.length; i++) {
+    if (
+      fruits.includes(value.toLowerCase()) &&
+      card[i].getAttribute("data-value") === value.toLowerCase()
+    ) {
+      card[i].setAttribute("class", "success");
+      rightAnswers = rightAnswers + 1;
+      console.log(rightAnswers);
+    }
+  }
+  if (rightAnswers === 9) {
+    window.location.href = "../Success/success.html";
+  }
+}
